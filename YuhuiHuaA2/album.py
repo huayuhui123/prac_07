0dfa3952eacf2072d93a2c3140c65d1e6ec5cb8f
yuhui Hua <2826537460@qq.com>
2021-11-06 21:33:24 +0800

    create __str__() function

--- YuhuiHuaA2/album.py
+++ YuhuiHuaA2/album.py
@@ -8,3 +8,8 @@ class Album:
         self.year=year
         self.is_completed=is_completed
 
+    def __str__(self):
+        if not self.is_completed:
+            print("*",end='')
+        return "{:<30} by {:<20}({})".format(self.title, self.artist, self.year)
+
