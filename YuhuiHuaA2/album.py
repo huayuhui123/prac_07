6431a482c29b08b53489a03f36aefddbb877a085
yuhui Hua <2826537460@qq.com>
2021-11-06 20:07:41 +0800

    initial commit

---
+++ YuhuiHuaA2/album.py
@@ -0,0 +1,10 @@
+"""..."""
+
+
+class Album:
+    def __init__(self,title,artist,year,is_completed):
+        self.title=title
+        self.artist=artist
+        self.year=year
+        self.is_completed=is_completed
+
