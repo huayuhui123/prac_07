7d2d4b5f0641808ab66fb59270008da22d9155af
yuhui Hua <2826537460@qq.com>
2021-11-07 15:59:58 +0800

    add the init,str,load method to this class,tested successfully

--- YuhuiHuaA2/albumcollection.py
+++ YuhuiHuaA2/albumcollection.py
@@ -1,5 +1,23 @@
 """..."""
 
+import csv
+
 
 class AlbumCollection:
-    pass
+    def __init__(self, albums=None):
+        self.albums = albums
+
+    def __str__(self):
+        return str(self.albums)
+
+    def load_albums(self, filename=None):
+        data_file = open(filename, "r")
+        self.albums = [line for line in csv.reader(open(filename))]
+        data_file.close()
+
+    def sort(self):
+        pass
+
+    def add_album(self):
+        pass
+
